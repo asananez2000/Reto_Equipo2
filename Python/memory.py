8ea007d6c5fbb4e9b9ed29bcd713914df515ab0d
Andreina Sananez <asananez2000@gmail.com>
2021-09-16 12:04:53 -0500

    Changed game to a 4x4 grid, and added comments

--- Python/memory.py
+++ Python/memory.py
@@ -9,16 +9,21 @@ Exercises:
 5. Use letters instead of tiles.
 
 """
-
+# Import necessary libraries
 from random import *
 from turtle import *
 from freegames import path
 
-car = path('car.gif')
-tiles = list(range(32)) * 2
-state = {'mark': None}
-hide = [True] * 64
+# Game elements
+grid_dim = 4 # grid dimension
+sqr_len = 400 / grid_dim # tile length
 
+car = path('car.gif')
+tiles = list(range(int(grid_dim**2 / 2))) * 2
+state = {'mark': None}
+hide = [True] * grid_dim**2
+
+# Function that draws one tile
 def square(x, y):
     "Draw white square with black outline at (x, y)."
     up()
@@ -26,45 +31,54 @@ def square(x, y):
     down()
     color('black', 'white')
     begin_fill()
+
+    # Draw each side of the square
     for count in range(4):
-        forward(50)
+        forward(sqr_len)
         left(90)
     end_fill()
 
+# Function that converts tile coordinates to an index
 def index(x, y):
     "Convert (x, y) coordinates to tiles index."
-    return int((x + 200) // 50 + ((y + 200) // 50) * 8)
+    return int((x + 200) // sqr_len + ((y + 200) // sqr_len) * grid_dim)
 
+# Function that converts tile count to coordinates
 def xy(count):
     "Convert tiles count to (x, y) coordinates."
-    return (count % 8) * 50 - 200, (count // 8) * 50 - 200
+    return (count % grid_dim) * sqr_len - 200, (count // grid_dim) * sqr_len - 200
 
+# Function that executes the necessary actions for a tile click or tap
 def tap(x, y):
     "Update mark and hidden tiles based on tap."
     spot = index(x, y)
-    mark = state['mark']
+    mark = state['mark'] 
 
     if mark is None or mark == spot or tiles[mark] != tiles[spot]:
-        state['mark'] = spot
+        state['mark'] = spot # change state of 'mark' to the tile's index
     else:
-        hide[spot] = False
+        hide[spot] = False # the index changes to a not hidden state
         hide[mark] = False
-        state['mark'] = None
+        state['mark'] = None # reset the state of 'mark'
 
+# Function that draws the background image and tiles 
 def draw():
     "Draw image and tiles."
+    # Draw background image
     clear()
     goto(0, 0)
     shape(car)
     stamp()
 
-    for count in range(64):
+    # Draw a white tile for each index that is hidden 
+    for count in range(grid_dim**2):
         if hide[count]:
             x, y = xy(count)
             square(x, y)
 
     mark = state['mark']
 
+    # If mark has an index and the tile is hidden, draw the corresponding number 
     if mark is not None and hide[mark]:
         x, y = xy(mark)
         up()
